17dc47324344eac08fc39398ca09dc5781b04acb
PabloSanchezB <pabloasb87@gmail.com>
2025-09-18 12:59:30 -0500

    Version Funcional 1

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let campoAmigo = document.querySelector('#amigo');
+
+    if(campoAmigo.value.trim() == ""){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        amigos.push(campoAmigo.value.trim());
+        campoAmigo.value = "";
+        actualizarListaAmigos();
+    }
+}
+
+function actualizarListaAmigos(){
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++){
+        listaAmigos.innerHTML += `<li>${amigos[i]}<li>`;
+    }
+}
+
+function sortearAmigo(){
+    if(amigos.length == 0){return;}
+
+    let indiceRandom = Math.floor(Math.random() * amigos.length);
+
+    let amigoSorteado = amigos[indiceRandom];
+    
+    document.querySelector('#resultado').innerHTML = `<li>Tu amigo secreto es: ${amigoSorteado}<li>`;
+}
